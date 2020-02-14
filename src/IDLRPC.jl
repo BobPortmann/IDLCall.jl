da199c90d35fd1466544b496c40870495e293c5e
hyzhou <hyzhou@umich.edu>
2020-02-14 17:56:12 -0500

    Use __init__() function for initializing the package.

--- src/IDLRPC.jl
+++ src/IDLRPC.jl
@@ -1,4 +1,3 @@
-
 include("idl_types.jl")
 include("common-funcs.jl")
 include("common-macros.jl")
@@ -14,32 +13,30 @@ RPCclient(ptr::Ptr{Nothing}) = RPCclient(ptr, nothing)
 pclient = RPCclient()
 
 function rpc_init()
-   ccall((:IDL_RPCInit, idlrpc), Ptr{Nothing}, (Clong, Ptr{UInt8}), 0, C_NULL)
+   ccall((:IDL_RPCInit, libidl_rpc), Ptr{Nothing}, (Clong, Ptr{UInt8}), 0, C_NULL)
 end
 
 function rpc_cleanup()
-   ecode = ccall((:IDL_RPCCleanup, idlrpc), Cint, (Ptr{Nothing}, Cint), pclient.ptr, 0)
-   if ecode != 1
-      error("IDL.exit: failed")
-   end
+   ecode = ccall((:IDL_RPCCleanup, libidl_rpc), Cint, (Ptr{Nothing}, Cint), pclient.ptr, 0)
+   ecode != 1 && error("IDL.exit: failed")
    if pclient.process != nothing
       kill(pclient.process)
    end
    return
 end
 
-function init()
+function __init__()
    olderr = stderr
-   (rd, wr) = redirect_stderr() #Redirect error messages
+   (rd, wr) = redirect_stderr() # Redirect error messages
    ptr = rpc_init()
-   if ptr != C_NULL #Check if idlrpc is already running
+   if ptr != C_NULL # Check if idlrpc is already running
       global pclient = RPCclient(ptr)
-   else #Start up idlrpc
-      print("Initializing IDL ")
-      proc = spawn(`idlrpc`)
+   else # Start up idlrpc
+      println("Initializing IDL")
+      run(`$idlrpc`, wait=false)
       ptr = C_NULL
       cnt = 0
-      while ptr == C_NULL && cnt < 60 #Allow for startup time
+      while ptr == C_NULL && cnt < 60 # Allow for startup time
          ptr = rpc_init()
          cnt = cnt + 1
          sleep(1)
@@ -49,19 +46,19 @@ function init()
       ptr == C_NULL && error("IDL.init: IDLRPC init failed")
       global pclient = RPCclient(ptr, proc)
    end
-   capture(true)
+   capture(true) # Capture output from IDL
    redirect_stderr(olderr)
-   #Register cleanup function to be called at exit
+   # Register cleanup function to be called at exit
    atexit(rpc_cleanup)
 end
 
 function execute_converted(str::AbstractString)
    # does no conversion of interpolated vars, continuation chars, or newlines
-   ecode = ccall((:IDL_RPCExecuteStr, idlrpc), Cint, (Ptr{Nothing},Ptr{UInt8}), pclient.ptr, str)
+   ecode = ccall((:IDL_RPCExecuteStr, libidl_rpc), Cint, (Ptr{Nothing},Ptr{UInt8}), pclient.ptr, str)
    if ecode != 1
       # since error get printed by IDL, we just reset error state
-      ecode = ccall((:IDL_RPCExecuteStr, idlrpc), Cint, (Ptr{Nothing},Ptr{UInt8}), pclient.ptr,
-      "message, /RESET")
+      ecode = ccall((:IDL_RPCExecuteStr, libidl_rpc), Cint, (Ptr{Nothing},Ptr{UInt8}), pclient.ptr,
+         "message, /RESET")
       flush()
       return false
    end
@@ -71,16 +68,14 @@ end
 
 function capture(flag::Bool)
    nlines = flag ? 5000 : 0
-   ecode = ccall((:IDL_RPCOutputCapture, idlrpc), Cint, (Ptr{Nothing}, Cint), pclient.ptr, nlines)
-   if ecode != 1
-      error("IDL.capture: IDL_RPCOutputCapture failed")
-   end
+   ecode = ccall((:IDL_RPCOutputCapture, libidl_rpc), Cint, (Ptr{Nothing}, Cint), pclient.ptr, nlines)
+   (ecode != 1) && error("IDL.capture: IDL_RPCOutputCapture failed")
    return nothing
 end
 
 function get_output!(line_s::IDL_RPC_LINE_S)
-   ecode = ccall((:IDL_RPCOutputGetStr, idlrpc), Cint, (Ptr{Nothing},Ref{IDL_RPC_LINE_S},Cint),
-   pclient.ptr, line_s, 0)
+   ecode = ccall((:IDL_RPCOutputGetStr, libidl_rpc), Cint, (Ptr{Nothing},Ref{IDL_RPC_LINE_S},Cint),
+      pclient.ptr, line_s, 0)
    ecode == 1 ? true : false
 end
 
@@ -95,7 +90,7 @@ function flush()
    end
 end
 
-# no free_cb needed in idlrpc (I think)
+# no free_cb needed in libidl_rpc (I think)
 free_cb = C_NULL
 
 # NOTE: Put_var makes a copy of the data in the array when it is put into idlrpc process.
@@ -108,15 +103,12 @@ function put_var(arr::Array{T,N}, name::AbstractString) where {T,N}
    end
    dim = zeros(Int, IDL_MAX_ARRAY_DIM)
    dim[1:N] = [size(arr)...]
-   vptr = ccall((:IDL_RPCImportArray, idlrpc), Ptr{IDL_Variable},
-   (Cint, IDL_ARRAY_DIM, Cint, Ptr{T}, IDL_ARRAY_FREE_CB),
+   vptr = ccall((:IDL_RPCImportArray, libidl_rpc), Ptr{IDL_Variable},
+      (Cint, IDL_ARRAY_DIM, Cint, Ptr{T}, IDL_ARRAY_FREE_CB),
    N, dim, idl_type(arr), arr, free_cb)
-   ecode = ccall((:IDL_RPCSetVariable, idlrpc), Cint,
-   (Ptr{Nothing}, Ptr{UInt8}, Ptr{IDL_Variable}),
-   pclient.ptr, name, vptr)
-   if ecode != 1
-      error("IDL.put_var: failed")
-   end
+   ecode = ccall((:IDL_RPCSetVariable, libidl_rpc), Cint,
+      (Ptr{Nothing}, Ptr{UInt8}, Ptr{IDL_Variable}), pclient.ptr, name, vptr)
+   ecode != 1 && error("IDL.put_var: failed")
    return
 end
 
@@ -140,12 +132,11 @@ function put_var(x, name::AbstractString)
       error("IDL.put_var: only works with some vars containing bits types")
    end
    vptr = get_vptr(name)
-   ccall((:IDL_RPCStoreScalar, idlrpc), Nothing,
-   (Ptr{IDL_Variable}, Cint, Ref{UInt128}),
+   ccall((:IDL_RPCStoreScalar, libidl_rpc), Nothing,
+      (Ptr{IDL_Variable}, Cint, Ref{UInt128}),
    vptr, idl_type(x), Ref{UInt128}(convert(UInt128,reinterpret(uint_size(x),x))))
-   ecode = ccall((:IDL_RPCSetVariable, idlrpc), Cint,
-   (Ptr{Nothing}, Ptr{UInt8}, Ptr{IDL_Variable}),
-   pclient.ptr, name, vptr)
+   ecode = ccall((:IDL_RPCSetVariable, libidl_rpc), Cint,
+      (Ptr{Nothing}, Ptr{UInt8}, Ptr{IDL_Variable}), pclient.ptr, name, vptr)
    return
 end
 
@@ -157,12 +148,10 @@ function put_var(x::Complex{T}, name::AbstractString) where {T}
       y = (convert(UInt128, reinterpret(UInt32, imag(x))) << 32) + reinterpret(UInt32, real(x))
    end
    vptr = get_vptr(name)
-   ccall((:IDL_RPCStoreScalar, idlrpc), Nothing,
-   (Ptr{IDL_Variable}, Cint, Ref{UInt128}),
-   vptr, idl_type(x), Ref{UInt128}(y))
-   ecode = ccall((:IDL_RPCSetVariable, idlrpc), Cint,
-   (Ptr{Nothing}, Ptr{UInt8}, Ptr{IDL_Variable}),
-   pclient.ptr, name, vptr)
+   ccall((:IDL_RPCStoreScalar, libidl_rpc), Nothing,
+      (Ptr{IDL_Variable}, Cint, Ref{UInt128}), vptr, idl_type(x), Ref{UInt128}(y))
+   ecode = ccall((:IDL_RPCSetVariable, libidl_rpc), Cint,
+      (Ptr{Nothing}, Ptr{UInt8}, Ptr{IDL_Variable}), pclient.ptr, name, vptr)
    return
 end
 
@@ -174,30 +163,27 @@ end
 
 function put_var(str::AbstractString, name::AbstractString)
    idl_string = IDL_String()
-   ccall((:IDL_RPCStrStore, idlrpc), Nothing, (Ptr{IDL_String}, Ptr{Cchar}),
-   idl_string, str)
-   ecode = ccall((:IDL_RPCSetVariable, idlrpc), Cint,
-   (Ptr{Nothing}, Ptr{UInt8}, Ptr{IDL_Variable}),
-   pclient.ptr, name, vptr)
-   if ecode != 1
-      error("IDL.put_var: failed")
-   end
+   ccall((:IDL_RPCStrStore, libidl_rpc), Nothing, (Ptr{IDL_String}, Ptr{Cchar}),
+      idl_string, str)
+   ecode = ccall((:IDL_RPCSetVariable, libidl_rpc), Cint,
+      (Ptr{Nothing}, Ptr{UInt8}, Ptr{IDL_Variable}), pclient.ptr, name, vptr)
+   ecode != 1 && error("IDL.put_var: failed")
 end
 
 function get_name(vptr::Ptr{IDL_Variable})
    # not implemented for RPC
-   str = ccall((:IDL_RPCVarName, idlrpc), Ptr{Cchar}, (Ptr{IDL_Variable},), vptr)
+   str = ccall((:IDL_RPCVarName, libidl_rpc), Ptr{Cchar}, (Ptr{IDL_Variable},), vptr)
    return unsafe_string(str)
 end
 
 function get_vptr(name::AbstractString)
    # returns C_NULL if name not in scope
-   ccall((:IDL_RPCGetMainVariable, idlrpc), Ptr{IDL_Variable}, (Ptr{Nothing},Ptr{UInt8}),
+   ccall((:IDL_RPCGetMainVariable, libidl_rpc), Ptr{IDL_Variable}, (Ptr{Nothing},Ptr{UInt8}),
    pclient.ptr, name)
 end
 
 function get_var(name::AbstractString)
-   vptr = ccall((:IDL_RPCGetMainVariable, idlrpc), Ptr{IDL_Variable}, (Ptr{Nothing},Ptr{UInt8}),
+   vptr = ccall((:IDL_RPCGetMainVariable, libidl_rpc), Ptr{IDL_Variable}, (Ptr{Nothing},Ptr{UInt8}),
    pclient.ptr, name)
    # NOTE: IDL_RPCGetVariable never seems to return NULL in spite of docs
    if vptr == C_NULL
@@ -205,6 +191,6 @@ function get_var(name::AbstractString)
    end
    var = get_var(vptr, name)
    # not sure if this is needed?
-   vptr = ccall((:IDL_RPCDeltmp, idlrpc), Nothing, (Ptr{IDL_Variable},), vptr)
+   vptr = ccall((:IDL_RPCDeltmp, libidl_rpc), Nothing, (Ptr{IDL_Variable},), vptr)
    var
 end

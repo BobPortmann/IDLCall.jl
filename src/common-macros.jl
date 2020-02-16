cc41578de661866a01c2feeea499929a1b6111d7
hyzhou <hyzhou@umich.edu>
2020-02-16 01:36:00 -0500

    Indentation adjustment.

--- src/common-macros.jl
+++ src/common-macros.jl
@@ -1,60 +1,58 @@
-
 # These routines were modified from similar routines in MATLAB.jl
 # E.G., @mput, @mget, _mput_multi, _mget_multi, make_getvar_statement
 
 function put_var_multi(vs::Symbol...)
-    nv = length(vs)
-    if nv == 1
-        v = vs[1]
-        :(put_var($(v), string($(Meta.quot(v)))))
-    else
-        stmts = Array(Expr, nv)
-        for i = 1:nv
-            v = vs[i]
-            stmts[i] = :(put_var($(v), string($(Meta.quot(v)))))
-        end
-        Expr(:block, stmts...)
-    end
+   nv = length(vs)
+   if nv == 1
+      v = vs[1]
+      :(put_var($(v), string($(Meta.quot(v)))))
+   else
+      stmts = Array(Expr, nv)
+      for i = 1:nv
+         v = vs[i]
+         stmts[i] = :(put_var($(v), string($(Meta.quot(v)))))
+      end
+      Expr(:block, stmts...)
+   end
 end
 
 macro put_var(vs...)
-    esc(put_var_multi(vs...))
+   esc(put_var_multi(vs...))
 end
 
 function make_getvar_statement(v::Symbol)
-    :($(v) = get_var(string($(Meta.quot(v)))))
+   :($(v) = get_var(string($(Meta.quot(v)))))
 end
 
 function make_getvar_statement(ex::Expr)
-    if !(ex.head == :(=))
-        if ex.head == :kw
-            error("Must call @get_var without parenthesis if using statements")
-        else
+   if !(ex.head == :(=))
+      if ex.head == :kw
+         error("Must call @get_var without parenthesis if using statements")
+         else
             erorr("Invalid expression for @get_var: " * string(ex))
-        end
-    end
-    v::Symbol = ex.args[1]
-    k::Symbol = ex.args[2]
-    :($(v) = get_var(string($(Meta.quot(k)))))
+         end
+      end
+      v::Symbol = ex.args[1]
+      k::Symbol = ex.args[2]
+      :($(v) = get_var(string($(Meta.quot(k)))))
+   end
 end
 
 function get_var_multi(vs::Union{Symbol, Expr}...)
-    # NOTE: I supressed output by adding :nothing
-    nv = length(vs)
-    if nv == 1
-        stmt = make_getvar_statement(vs[1])
-        Expr(:block, stmt, :nothing)
-    else
-        stmts = Array(Expr, nv)
-        for i = 1:nv
-            stmts[i] = make_getvar_statement(vs[i])
-        end
-        Expr(:block, stmts..., :nothing)
-    end
+   # supress output by adding :nothing
+   nv = length(vs)
+   if nv == 1
+      stmt = make_getvar_statement(vs[1])
+      Expr(:block, stmt, :nothing)
+   else
+      stmts = Array(Expr, nv)
+      for i = 1:nv
+         stmts[i] = make_getvar_statement(vs[i])
+      end
+      Expr(:block, stmts..., :nothing)
+   end
 end
 
 macro get_var(vs...)
-    esc(get_var_multi(vs...))
+   esc(get_var_multi(vs...))
 end
-
-# END: routines modified from similar routines in MATLAB.jl

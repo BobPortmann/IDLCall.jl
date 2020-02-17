804474f1d925279e706bac26f644aa0a2897c49e
hyzhou <hyzhou@umich.edu>
2020-02-17 09:26:17 -0500

    Fix active_repl not found for testing.

--- src/IDLREPL.jl
+++ src/IDLREPL.jl
@@ -1,13 +1,14 @@
 import REPL:respond, LineEdit, mode_keymap
-import Base:active_repl, text_colors
 
 function idl_repl()
     # Setup idl prompt
     prompt = LineEdit.Prompt("IDL> ";
-    prompt_prefix=text_colors[:blue],
-    prompt_suffix=text_colors[:white])
+        prompt_prefix=Base.text_colors[:blue],
+        prompt_suffix=Base.text_colors[:white])
 
-    repl = active_repl
+    !isdefined(Base, :active_repl) && return
+
+    repl = Base.active_repl
 
     prompt.on_done = respond(repl,prompt) do line
         ok2, line, msg = convert_continuations(line)
@@ -41,27 +42,27 @@ function idl_repl()
     prompt.hist = hp
 
     idl_keymap = Dict{Any,Any}(
-    '>' => function (s,args...)
-    if isempty(s)
-        if !haskey(s.mode_state,prompt)
-            s.mode_state[prompt] = LineEdit.init_state(repl.t,prompt)
+        '>' => function (s,args...)
+        if isempty(s)
+            if !haskey(s.mode_state,prompt)
+                s.mode_state[prompt] = LineEdit.init_state(repl.t,prompt)
+            end
+            LineEdit.transition(s,prompt)
+        else
+            LineEdit.edit_insert(s,'>')
         end
-        LineEdit.transition(s,prompt)
-    else
-        LineEdit.edit_insert(s,'>')
-    end
-end)
+    end)
 
-search_prompt, skeymap = LineEdit.setup_search_keymap(hp)
-mk = mode_keymap(main_mode)
+    search_prompt, skeymap = LineEdit.setup_search_keymap(hp)
+    mk = mode_keymap(main_mode)
 
-b = Dict{Any,Any}[skeymap, mk, LineEdit.history_keymap,
-LineEdit.default_keymap, LineEdit.escape_defaults]
-prompt.keymap_dict = LineEdit.keymap(b)
+    b = Dict{Any,Any}[skeymap, mk, LineEdit.history_keymap,
+        LineEdit.default_keymap, LineEdit.escape_defaults]
+        prompt.keymap_dict = LineEdit.keymap(b)
 
-main_mode.keymap_dict = LineEdit.keymap_merge(main_mode.keymap_dict,
-idl_keymap)
-nothing
+    main_mode.keymap_dict = LineEdit.keymap_merge(main_mode.keymap_dict,
+        idl_keymap)
+    nothing
 end
 
 function find_prompt_in_modes(modes, name)
